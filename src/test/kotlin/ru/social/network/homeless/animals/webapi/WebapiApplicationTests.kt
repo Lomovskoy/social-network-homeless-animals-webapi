0443236eda616db2e57399237ccde6b1d1618c95
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-07-26 18:19:05 +0300

    #1 Создал шаблон приложения.

---
+++ src/test/kotlin/ru/social/network/homeless/animals/webapi/WebapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.social.network.homeless.animals.webapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
